264a47453c5833fef683b128e5b87e7e3af83246
Aidan Delaney <aidan@phoric.eu>
2011-12-05 10:42:25 +0000

    * In class solution to BoundingBox collision detection

--- src/BoundingBox.cpp
+++ src/BoundingBox.cpp
@@ -7,6 +7,46 @@ BoundingBox::BoundingBox(const Point3 & c, const float extent_x, const float ext
   this->h_z = Vector3(0.0, 0.0, extent_z/2.0);
 }
 
-bool BoundingBox::collidesWith(const BoundingBox & b) {
+bool between (const pair<float, float> & a, const pair<float, float> & b) {
+  if((a.first >= b.first && a.first <= b.second)
+     || (a.second >= b.first && a.second <= b.second))
+    {
+      return true;
+    }
   return false;
+}
+
+bool BoundingBox::collidesWith(const BoundingBox & b) {
+
+  if(between(projectOntoAxis(*this, X), projectOntoAxis(b, X))
+     && between(projectOntoAxis(*this, Y), projectOntoAxis(b, Y))
+     && between(projectOntoAxis(*this, Z), projectOntoAxis(b, Z))
+     )
+    {
+      return true;
+    }
+
+  return false;
+}
+
+pair<float,float> BoundingBox::projectOntoAxis(const BoundingBox & b, enum AXIS axis) {
+
+  float lo, hi;
+
+  switch (axis) {
+  case X:
+    lo  = projection( Point3(Vector3(b.c) + (-1 * b.h_x)), UNIT_X_AXIS);
+    hi  = projection( Point3(Vector3(b.c) + b.h_x), UNIT_X_AXIS);
+    break;
+  case Y:
+    lo  = projection( Point3(Vector3(b.c) + (-1 * b.h_y)), UNIT_Y_AXIS);
+    hi  = projection( Point3(Vector3(b.c) + b.h_y), UNIT_Y_AXIS);
+    break;
+  case Z:
+    lo  = projection( Point3(Vector3(b.c) + (-1 * b.h_z)), UNIT_Z_AXIS);
+    hi  = projection( Point3(Vector3(b.c) + b.h_z), UNIT_Z_AXIS);
+    break;
+  }
+
+  return make_pair(lo, hi);
 }
