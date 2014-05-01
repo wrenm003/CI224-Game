faa7501afeb06dcc5af4b66f1f5a0a663cc0b347
wrenm003 <wrenm003@gmail.com>
2014-05-01 13:36:11 +0100

    Initial commit, removed md2, added cube

---
+++ src/Cube.cpp
@@ -0,0 +1,66 @@
+#include "Cube.h"
+
+Cube::Cube(): GameAsset()
+{
+  Cube(0, 0, 0);
+}
+
+Cube::Cube(float x, float y, float z) {
+  //this->li = nullptr;
+
+  // A default unit cube
+  num_vertices = 8;
+  num_triangles = 12;
+  g_vertex_buffer_data = new GLfloat[num_vertices * 3]{
+
+//x     y    z
+-0.5, -0.5,  0.5, //F - 0
+ 0.5, -0.5,  0.5, //F - 1
+-0.5,  0.5,  0.5, //F - 2
+ 0.5,  0.5,  0.5, //F - 3
+-0.5, -0.5, -0.5, //B - 4
+ 0.5, -0.5, -0.5, //B - 5
+-0.5,  0.5, -0.5, //B - 6
+ 0.5,  0.5, -0.5  //B - 7
+}; // three points per vertex
+
+  g_element_buffer_data = new GLushort[num_triangles * 3]{
+
+F0, F1, F2, //front
+F1, F3, F2,
+
+F1, R2, F3, //right
+R2, R4, F3,
+
+R2, R1, R4, //back
+R1, R3, R4,
+
+R1, F0, R3, //left
+F0, F2, R3,
+
+F2, F3, R3, //top
+F3, R4, R3,
+
+R1, R2, F0, //bottom
+R2, F1, F0
+
+}; // three vertices per triangle
+
+  bbox.reset();
+  bbox = shared_ptr<BoundingBox>(new BoundingBox(Point3(x, y, z), 1.0, 1.0, 1.0));
+
+  make_resources();
+}
+
+Cube::~Cube() {
+  // TODO: do something nice and fun here.
+}
+
+void Cube::update() {
+}
+
+void Cube::draw() {
+  GameAsset::draw();
+}
+
+void Cube::clean() { } 
---
+++ src/Cube.h
@@ -0,0 +1,22 @@
+#include "GameAsset.h"
+
+#ifndef Cube_H_
+#define Cube_H_
+
+class Cube : public GameAsset {
+ public:
+  Cube();
+  Cube(float x, float y, float z);
+    ~Cube();
+
+  virtual void update();
+  virtual void draw();
+  virtual void clean();
+
+  enum vertices {
+F0, F1, F2, F3, R1, R2, R3 , R4, };
+
+ private:
+};
+
+#endif // Cube_H_
